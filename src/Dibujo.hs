cde768097e292a57aa45e79b4fe661b768b9bf68
JoseIvanNieva <i.nieva@mi.unc.edu.ar>
2024-03-24 11:07:25 -0300

    Implementacion del TAD en Dibujo.hs

--- src/Dibujo.hs
+++ src/Dibujo.hs
@@ -1,10 +1,16 @@
-module Dibujo (encimar, 
+module Dibujo (Dibujo, figura, rotar, espejar, 
+              rot45, juntar, encimar, r180, r270,r90,
+              (///), (^^^), (.-.), encimar4, ciclar,
+              cuarteto, mapDib, foldDib,change
     -- agregar las funciones constructoras
-    ) where
+) where
 
 
 -- nuestro lenguaje 
-data Dibujo a = Dibujo
+data Dibujo a = Basica a|Rotar (Dibujo a) |Espejar (Dibujo a)|
+                Rot45 (Dibujo a)|Apilar Float Float (Dibujo a) (Dibujo a)|
+                Juntar Float Float (Dibujo a) (Dibujo a)|
+                Encimar (Dibujo a) (Dibujo a) deriving(Eq, Show)
 
 -- combinadores
 infixr 6 ^^^
@@ -14,73 +20,89 @@ infixr 7 .-.
 infixr 8 ///
 
 comp :: Int -> (a -> a) -> a -> a
-comp n f = undefined
+comp 0 f x = f x
+comp n f x = f (comp (n-1) f x)
 
 
 -- Funciones constructoras
 figura :: a -> Dibujo a
-figura = undefined
+figura = Basica
 
 encimar :: Dibujo a -> Dibujo a -> Dibujo a
-encimar = undefined
+encimar = Encimar
 
 apilar :: Float -> Float -> Dibujo a -> Dibujo a -> Dibujo a
-apilar = undefined
+apilar = Apilar
 
 juntar  :: Float -> Float -> Dibujo a -> Dibujo a -> Dibujo a
 juntar = Juntar
 
 rot45 :: Dibujo a -> Dibujo a
-rot45 = undefined
+rot45 = Rot45
 
 rotar :: Dibujo a -> Dibujo a
-rotar = undefined
+rotar = Rotar
 
 
 espejar :: Dibujo a -> Dibujo a
-espejar = undefined
+espejar = Espejar
 
 (^^^) :: Dibujo a -> Dibujo a -> Dibujo a
-(^^^) = undefined
+(^^^) d1 d2 = encimar d1 d2
 
 (.-.) :: Dibujo a -> Dibujo a -> Dibujo a
-(.-.) = undefined
+(.-.) d1 d2 = apilar  1 1 d1 d2 
 
 (///) :: Dibujo a -> Dibujo a -> Dibujo a
-(///) = undefined
+(///) d1 d2 = juntar 1 1 d1 d2
 
 -- rotaciones
 r90 :: Dibujo a -> Dibujo a
-r90 = undefined
+r90 = rotar
 
 r180 :: Dibujo a -> Dibujo a
-r180 = undefined
+r180 = comp 1 rotar 
 
 r270 :: Dibujo a -> Dibujo a
-r270 = undefined
+r270 = comp 2 rotar
 
 -- una figura repetida con las cuatro rotaciones, superimpuestas.
 encimar4 :: Dibujo a -> Dibujo a
-encimar4 = undefined
+encimar4 d = (^^^) ((^^^) d (r90 d)) ((^^^) (r180 d) (r270 d))
 
 -- cuatro figuras en un cuadrante.
 cuarteto :: Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a
-cuarteto = undefined
+cuarteto d1 d2 d3 d4 = (.-.) ((///) d1 d2) ((///) d3 d4)
 
 -- un cuarteto donde se repite la imagen, rotada (¡No confundir con encimar4!)
 ciclar :: Dibujo a -> Dibujo a
-ciclar = undefined
+ciclar d = cuarteto d d1 d2 d3 
+          where d1 = rotar d
+                d2 = r180 d
+                d3 = r270 d
 
 -- map para nuestro lenguaje
 mapDib :: (a -> b) -> Dibujo a -> Dibujo b
-mapDib = undefined
+mapDib f (Basica a)         = Basica (f a)
+mapDib f (Rotar d)          = Rotar (mapDib f d) 
+mapDib f (Espejar d)        = Espejar (mapDib f d) 
+mapDib f (Rot45 d)          = Rot45 (mapDib f d) 
+mapDib f (Apilar n m d1 d2) = Apilar n m (mapDib f d1) (mapDib f d2)
+mapDib f (Juntar n m d1 d2) = Juntar n m (mapDib f d1) (mapDib f d2)
+mapDib f (Encimar d1 d2)    = Encimar (mapDib f d1) (mapDib f d2) 
 -- verificar que las operaciones satisfagan
 -- 1. map figura = id
 -- 2. map (g . f) = mapDib g . mapDib f
 
 -- Cambiar todas las básicas de acuerdo a la función.
 change :: (a -> Dibujo b) -> Dibujo a -> Dibujo b
-change = undefined
+change f (Basica c)         = f c 
+change f (Rotar d)          = Rotar (change f d) 
+change f (Espejar d)        = Espejar (change f d)
+change f (Rot45 d)          = Rot45 (change f d)
+change f (Apilar n m d1 d2) = Apilar n m (change f d1) (change f d2)
+change f (Juntar n m d1 d2) = Juntar n m (change f d1) (change f d2)
+change f (Encimar d1 d2)    = Encimar (change f d1) (change f d2)
 
 -- Principio de recursión para Dibujos.
 foldDib ::
