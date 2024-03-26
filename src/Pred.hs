526bd02aefce8753d72eb15424976a23f123be92
JoseIvanNieva <i.nieva@mi.unc.edu.ar>
2024-03-26 20:06:46 -0300

    andP y orP implementadas

--- src/Pred.hs
+++ src/Pred.hs
@@ -10,8 +10,8 @@ type Pred a = a -> Bool
 -- Dado un predicado sobre básicas, cambiar todas las que satisfacen
 -- el predicado por la figura básica indicada por el segundo argumento.
 cambiar :: Pred a -> (a -> Dibujo a) -> Dibujo a -> Dibujo a
-cambiar pred fun  = change pred'
-                  where pred' n = if pred n then fun n else figura n 
+cambiar p f  = change fun
+                  where fun n = if p n then f n else figura n 
 
 -- Alguna básica satisface el predicado.
 anyDib :: Pred a -> Dibujo a -> Bool
@@ -19,13 +19,17 @@ anyDib pred = foldDib pred id id id ordFold ordFold (||)
               where ordFold _ _  = (||)  
 
 -- Todas las básicas satisfacen el predicado.
-allDib = undefined
+allDib :: Pred a -> Dibujo a -> Bool
+allDib pred = foldDib pred id id id andFold andFold (&&)
+              where andFold _ _ = (&&)
 
 -- Los dos predicados se cumplen para el elemento recibido.
-andP = undefined
+andP :: Pred a -> Pred a -> Pred a
+andP p1 p2 a = (p1 a) && (p2 a) 
 
 -- Algún predicado se cumple para el elemento recibido.
-orP = undefined
+orP :: Pred a -> Pred a -> Pred a
+orP p1 p2 a = (p1 a) || (p2 a)
 
 falla = True
 
